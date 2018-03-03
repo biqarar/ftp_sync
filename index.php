cbc3ba7d0c1a2e8532a49aeea2a46849a1b0f133
Reza Mohiti <rm.biqarar@gmail.com>
2018-03-03 11:31:39 +0330

    fix emample

--- index.php
+++ index.php
@@ -1,17 +1,18 @@
 <?php
 require_once('ftp_sync.php');
 
-ftp_sync::$ftp_host  = 'example.com';
-ftp_sync::$ftp_user  = 'user';
-ftp_sync::$ftp_pass  = '123';
+ftp_sync::$ftp_host  = '__YOUR FTP DOMAIN__';
+ftp_sync::$ftp_user  = '__YOUR FTP USER__';
+ftp_sync::$ftp_pass  = '__YOUR FTP PASSWORD__';
 ftp_sync::$ftp_port  = 21;
-ftp_sync::$ftp_path  = 'reza/1/';
-ftp_sync::$directory = '/home/reza/project/dash/';
+ftp_sync::$ftp_path  = '__YOUR FTP PATH DIRECTORY__[reza/1/ FOR EXAMPLE]';
+ftp_sync::$directory = '__YOUR LOCAL PATH DIRECTORY__[/home/reza/1/ FOR EXAMPLE]';
 ftp_sync::$debug     = true;
 
 $result = ftp_sync::run();
 if(!$result)
 {
-	print_r(ftp_sync::error());
+	// show errors
+	// print_r(ftp_sync::error());
 }
 ?>
